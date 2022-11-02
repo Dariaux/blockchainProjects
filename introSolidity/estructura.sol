73b26af441091af3a1fb68eeca4d6ab5442f6a3a
angel hernandez <angel.hernandez_santiago@telus.com>
2022-11-02 08:42:35 -0600

    usando tipos de variables en solidity

--- introSolidity/estructura.sol
+++ introSolidity/estructura.sol
@@ -3,7 +3,15 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract estructura { 
-    constructor () { 
-        
+
+    int cantidad;
+    uint cantidadSinSigno;
+    address direccion;
+    bool firmado;
+    
+    constructor (bool estaFirmado) { 
+        direccion = msg.sender; // Esto hace referencia al usuario que esta creando el contrato.
+        firmado = estaFirmado; // Recibimos el valor por parametro y asi sabremos si esta firmado o no el contrato.
+
     }
 }
